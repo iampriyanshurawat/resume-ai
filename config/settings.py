2858e00fc60c1cd557975af87bddef5f04c194a4
Priyanshu Rawat <iampriyanshurawat@gmail.com>
2026-02-08 23:57:06 +0530

    Fixed react cors allowed origin url

--- config/settings.py
+++ config/settings.py
@@ -64,8 +64,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://resume-6j16gv0cz-priyanshu-rawats-projects-7ece73a1.vercel.app/",
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
 ]
 
 ROOT_URLCONF = 'config.urls'
